5dc7634d27ae34880b41615f609036594b495687
Trisha Hadley <trisha.hadley@gmail.com>
2014-04-16 23:42:38 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Breath::Application.config.secret_key_base = 'b0b6855fab3a57d95391a9e538e43ddcce5fe0f96f583a4a19a95f2f66fa9f59f4abade5ca97ba4be26cd1f0dc9c81b4941f52e514510e22f05ec13fd430dce1'
