a1ad03e624570b9d4c235f73f56866b44ae61ae0
Trisha Hadley <trisha.hadley@gmail.com>
2014-04-22 17:04:13 -0500

    Implemented ActiveAdmin, updated user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Breath::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
   get "welcome/about"
   get "welcome/launch"
