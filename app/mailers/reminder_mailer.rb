c96c50768725e4e30df8cc973f292fc3ebfe6cd0
Trisha Hadley <trisha.hadley@gmail.com>
2014-05-17 00:21:11 -0500

    Created additional email templates

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -8,7 +8,7 @@ class ReminderMailer < ActionMailer::Base
 
   def reminder_email_2(email)
     @url = 'http://onebreath.io'
-    mail(to: email, subject: 'Breathe')
+    mail(to: email, subject: 'Yes')
   end
 
   def reminder_email_3(email)
@@ -23,6 +23,21 @@ class ReminderMailer < ActionMailer::Base
 
   def reminder_email_5(email)
     @url = 'http://onebreath.io'
+    mail(to: email, subject: ':)')
+  end
+
+    def reminder_email_6(email)
+    @url = 'http://onebreath.io'
     mail(to: email, subject: 'Breathe')
+  end
+
+  def reminder_email_7(email)
+    @url = 'http://onebreath.io'
+    mail(to: email, subject: 'Why yes')
+  end
+
+  def reminder_email_8(email)
+    @url = 'http://onebreath.io'
+    mail(to: email, subject: ':)')
   end
 end
