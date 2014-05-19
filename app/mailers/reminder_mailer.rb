f6b06850e64e99bc6917b2fee7b0f6384bb8b361
Trisha Hadley <trisha.hadley@gmail.com>
2014-05-19 15:36:22 -0500

    Cleaned up sms messages for better UX

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -45,26 +45,26 @@ class ReminderMailer < ActionMailer::Base
   # SMS templates (no subject)
   def reminder_sms(email)
     @url = 'http://onebreath.io'
-    mail(to: email, subject: '')
+    mail(to: email, subject: '', from: '_onebreath')
   end
 
   def reminder_sms_2(email)
     @url = 'http://onebreath.io'
-    mail(to: email, subject: '')
+    mail(to: email, subject: '', from: '_onebreath')
   end
 
   def reminder_sms_3(email)
     @url = 'http://onebreath.io'
-    mail(to: email, subject: '')
+    mail(to: email, subject: '', from: '_onebreath')
   end
 
   def reminder_sms_4(email)
     @url = 'http://onebreath.io'
-    mail(to: email, subject: '')
+    mail(to: email, subject: '', from: '_onebreath')
   end
 
   def reminder_sms_5(email)
     @url = 'http://onebreath.io'
-    mail(to: email, subject: '')
+    mail(to: email, subject: '', from: '_onebreath')
   end
 end
