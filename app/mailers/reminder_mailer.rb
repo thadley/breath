0e1287dbda22a10e2ea2ac429a9a645dd6b13a02
Trisha Hadley <trisha.hadley@gmail.com>
2014-06-17 02:02:23 -0500

    Added instagram feature to email message

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -4,51 +4,61 @@ class ReminderMailer < ActionMailer::Base
   # email templates
   def reminder_email(email)
     @url = 'http://onebreath.io'
+    @instagram = 'instagram://camera'
     mail(to: email, subject: 'Breathe')
   end
 
   def reminder_email_2(email)
     @url = 'http://onebreath.io'
+    @instagram = 'instagram://camera'
     mail(to: email, subject: '--')
   end
 
   def reminder_email_3(email)
     @url = 'http://onebreath.io'
+    @instagram = 'instagram://camera'
     mail(to: email, subject: '--')
   end
 
   def reminder_email_4(email)
     @url = 'http://onebreath.io'
+    @instagram = 'instagram://camera'
     mail(to: email, subject: 'Breathe')
   end
 
   def reminder_email_5(email)
     @url = 'http://onebreath.io'
+    @instagram = 'instagram://camera'
     mail(to: email, subject: ':)')
   end
 
   def reminder_email_6(email)
     @url = 'http://onebreath.io'
+    @instagram = 'instagram://camera'
     mail(to: email, subject: '--')
   end
 
   def reminder_email_7(email)
     @url = 'http://onebreath.io'
+    @instagram = 'instagram://camera'
     mail(to: email, subject: '--')
   end
 
   def reminder_email_8(email)
     @url = 'http://onebreath.io'
+    @instagram = 'instagram://camera'
     mail(to: email, subject: '--')
   end
 
   def reminder_email_9(email)
     @url = 'http://onebreath.io'
+    @instagram = 'instagram://camera'
     mail(to: email, subject: '--')
   end
 
   def reminder_email_10(email)
     @url = 'http://onebreath.io'
+    @instagram = 'instagram://camera'
     mail(to: email, subject: '--')
   end
 
