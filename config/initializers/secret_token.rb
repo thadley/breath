263a6660b8cb300a635d1206217cf94fac9e01a9
Trisha Hadley <trisha.hadley@gmail.com>
2014-07-02 14:45:04 -0500

    Generated new secret token and secured privacy

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Breath::Application.config.secret_key_base = 'b0b6855fab3a57d95391a9e538e43ddcce5fe0f96f583a4a19a95f2f66fa9f59f4abade5ca97ba4be26cd1f0dc9c81b4941f52e514510e22f05ec13fd430dce1'
+Breath::Application.config.secret_key_base = ENV['SECRET_TOKEN']
